6117b82649b927c36b2abc92f8d8dd57084e1f56
谢明 <ming.xie@socialcredits.cn>
2018-08-17 17:43:32 +0800

    fix: 格式化代码

--- compressMe.py
+++ compressMe.py
@@ -1,47 +1,50 @@
-#run this in any directory add -v for verbose 
-#get Pillow (fork of PIL) from pip before running --> pip install Pillow
+# run this in any directory add -v for verbose
+# get Pillow (fork of PIL) from pip before running --> pip install Pillow
 
 import os
 import sys
 from PIL import Image
 
+
 def compressMe(file, verbose=False):
-	filepath = os.path.join(os.getcwd(), file)
-	oldsize = os.stat(filepath).st_size
-	picture = Image.open(filepath)
-	dim = picture.size
-	width = dim[0]
-	height = dim[1]
-	#resize image
-	picture = picture.resize((280, (280*height)/width),Image.ANTIALIAS)
-	#set quality= to the preferred quality. 
-	#I found that 85 has no difference in my 6-10mb files and that 65 is the lowest reasonable number
-	picture.save("preview_"+file,"JPEG",optimize=True,quality=85) 
-	
-	newsize = os.stat(os.path.join(os.getcwd(),"preview_"+file)).st_size
-	percent = (oldsize-newsize)/float(oldsize)*100
-	if (verbose):
-		print "File compressed from {0} to {1} or {2}%".format(oldsize,newsize,percent)
-	return percent
+    filepath = os.path.join(os.getcwd(), file)
+    oldsize = os.stat(filepath).st_size
+    picture = Image.open(filepath)
+    dim = picture.size
+    width = dim[0]
+    height = dim[1]
+    # resize image
+    picture = picture.resize((280, (280 * height) / width), Image.ANTIALIAS)
+    # set quality= to the preferred quality.
+    # I found that 85 has no difference in my 6-10mb files and that 65 is the lowest reasonable number
+    picture.save("preview_" + file, "JPEG", optimize=True, quality=85)
+
+    newsize = os.stat(os.path.join(os.getcwd(), "preview_" + file)).st_size
+    percent = (oldsize - newsize) / float(oldsize) * 100
+    if (verbose):
+        print "File compressed from {0} to {1} or {2}%".format(oldsize, newsize, percent)
+    return percent
+
 
 def main():
-	verbose = False
-	#checks for verbose flag
-	if (len(sys.argv)>1):
-		if (sys.argv[1].lower()=="-v"):
-			verbose = True
+    verbose = False
+    # checks for verbose flag
+    if (len(sys.argv) > 1):
+        if (sys.argv[1].lower() == "-v"):
+            verbose = True
 
-	#finds present working dir
-	pwd = os.getcwd()
-	
-	tot = 0
-	num = 0
-	for file in os.listdir(pwd):
-		if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg'):
-			num += 1
-			tot += compressMe(file, verbose)
-	print "Average Compression: %d" % (float(tot)/num)
-	print "Done"
+    # finds present working dir
+    pwd = os.getcwd()
+
+    tot = 0
+    num = 0
+    for file in os.listdir(pwd):
+        if os.path.splitext(file)[1].lower() in ('.jpg', '.jpeg'):
+            num += 1
+            tot += compressMe(file, verbose)
+    print "Average Compression: %d" % (float(tot) / num)
+    print "Done"
+
 
 if __name__ == "__main__":
-	main()
+    main()
